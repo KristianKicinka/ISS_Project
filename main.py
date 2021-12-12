f175c75fa19c3bf4c9b14a4006239e85d7c0a741
KristianKicinka <kristian.kicinka@gmail.com>
2021-12-12 14:28:10 +0100

    After add task 2

--- main.py
+++ main.py
@@ -8,10 +8,15 @@ from scipy.signal import spectrogram, lfilter, freqz, tf2zpk
 
 
 def load_sound_file():
-    global signal, sampl_frekv, time_line
+    global signal, sampl_frekv, time_line, time
+
     signal, sampl_frekv = sf.read('xkicin02.wav')
-    signal = signal[:250000]
     time_line = np.arange(signal.size) / sampl_frekv
+    time = signal.size / sampl_frekv
+
+    print(f'count of samples : {signal.size}')
+    print(f'FS is : {sampl_frekv}')
+    print(f'time of recordes sound : {time} ')
 
 def show_input_graph():
     plt.figure(figsize=(8, 5))
@@ -19,6 +24,16 @@ def show_input_graph():
     plt.ylabel('value')
     plt.gca().set_xlabel('time [s]')
     plt.gca().set_title('Vstupný zvukový signál')
+    plt.tight_layout()
+    plt.show()
+
+def show_frames_graph():
+    plt.figure(figsize=(30, 8))
+    time_line = np.arange(frames[7].size) /sampl_frekv
+    plt.plot(time_line, frames[7])
+    plt.ylabel('value')
+    plt.gca().set_xlabel('time [s]')
+    plt.gca().set_title('Rámce')
     plt.tight_layout()
     plt.show()
 
@@ -30,6 +45,13 @@ def task_1():
     print(f'Max value is : {max_val}')
     print(f'Min value is : {min_val}')
 
+def task_2():
+    # choose correct frame from signal
+    global frames
+    frames = list(sf.blocks('xkicin02.wav',blocksize=1024,overlap=512))
+    show_frames_graph()
+
 if __name__ == '__main__':
     load_sound_file()
     task_1()
+    task_2()
