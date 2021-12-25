3638795b5fecef725fc0bac7b5dcfb363c6486b0
KristianKicinka <kristian.kicinka@gmail.com>
2021-12-25 21:43:46 +0100

    after update main file

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import matplotlib as matplotlib
-import numpy as np
-import matplotlib.pyplot as plt
-import soundfile as sound_file
-import IPython
-from scipy.signal import spectrogram, lfilter, freqz, tf2zpk
+
 from src.task_01 import *
 from src.task_02 import *
 from src.task_03 import *
@@ -14,28 +9,27 @@ from src.task_07 import *
 from src.task_10 import *
 
 
-
 def load_sound_file():
-    global signal, sample_frekvency, time_line, time
-    signal, sample_frekvency = sound_file.read('audio/xkicin02.wav')
-    time_line = np.arange(signal.size) / sample_frekvency
-    time = signal.size / sample_frekvency
+    global signal, sample_frequency, time_line, time
+    signal, sample_frequency = sound_file.read('audio/xkicin02.wav')
+    time_line = np.arange(signal.size) / sample_frequency
+    time = signal.size / sample_frequency
 
     print(f'count of samples : {signal.size}')
-    print(f'FS is : {sample_frekvency}')
-    print(f'time of recordes sound : {time} ')
+    print(f'FS is : {sample_frequency}')
+    print(f'time of records sound : {time} ')
 
 
 if __name__ == '__main__':
-    global signal, sample_frekvency, time, frames, found_frekvencies, time_line
+    global signal, sample_frequency, time, frames, found_frequencies, time_line
     load_sound_file()
     task_1(time_line, signal)
     frames = get_frames()
-    my_frame = task_2(frames, time_line, sample_frekvency)
+    my_frame = task_2(frames, time_line, sample_frequency)
     my_frame = task_3(my_frame)
-    task_4(signal, sample_frekvency)
-    found_frekvencies = task_5(my_frame, signal)
-    task_6(found_frekvencies, sample_frekvency, signal)
-    clear_signal = task_7(found_frekvencies, sample_frekvency, signal)
-    task_10(clear_signal, sample_frekvency)
+    task_4(signal, sample_frequency)
+    found_frequencies = task_5(my_frame, signal)
+    task_6(found_frequencies, sample_frequency, signal)
+    clear_signal = task_7(found_frequencies, sample_frequency, signal)
+    task_10(clear_signal, sample_frequency)
 
