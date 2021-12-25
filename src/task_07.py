8b72121ef1a581015d3f11fdd952e73ff7f1a8f6
KristianKicinka <kristian.kicinka@gmail.com>
2021-12-25 22:05:42 +0100

    after update task_07 file

--- src/task_07.py
+++ src/task_07.py
@@ -1,29 +1,26 @@
-import numpy as np
-import scipy.signal
-import soundfile
-from matplotlib import pyplot as plt
-from scipy.signal import spectrogram, lfilter
 
+import scipy.signal
+from scipy.signal import spectrogram
 from src.task_08 import *
 from src.task_09 import *
 
 
-def filter_function(frekvency, sample_frekvency, data, index):
+def filter_function(frequency, sample_frequency, data, index):
     pass_correction = 15
     stop_correction = 50
     g_pass = 3
     g_stop = 40
-    nyquest_frekv = sample_frekvency/2
+    nyquest_frequency = sample_frequency/2
 
-    pass_start = (frekvency-pass_correction)
-    pass_end = (frekvency+pass_correction)
+    pass_start = (frequency-pass_correction)
+    pass_end = (frequency+pass_correction)
     stop_start = (pass_start-stop_correction)
     stop_end = (pass_end+stop_correction)
 
-    pass_start = pass_start/nyquest_frekv
-    pass_end = pass_end/nyquest_frekv
-    stop_start = stop_start/nyquest_frekv
-    stop_end = stop_end/nyquest_frekv
+    pass_start = pass_start/nyquest_frequency
+    pass_end = pass_end/nyquest_frequency
+    stop_start = stop_start/nyquest_frequency
+    stop_end = stop_end/nyquest_frequency
 
     N, Wn = scipy.signal.buttord([stop_start, stop_end], [pass_start, pass_end], g_pass, g_stop, False)
 
@@ -35,14 +32,14 @@ def filter_function(frekvency, sample_frekvency, data, index):
 
     impulse_response(b, a, index)
     task_8(b, a, index)
-    task_9(b, a, sample_frekvency, index)
+    task_9(b, a, sample_frequency, index)
 
     filtered_signal = scipy.signal.filtfilt(b, a, data)
     return filtered_signal
 
 
-def task_7_spectogram(signal,sample_frekvency):
-    f, t, sgr = spectrogram(signal, sample_frekvency, nperseg=1024, noverlap=512)
+def task_7_spectogram(signal, sample_frequency):
+    f, t, sgr = spectrogram(signal, sample_frequency, nperseg=1024, noverlap=512)
     sgr_log = 10 * np.log10(pow(np.abs(sgr), 2))
     plt.figure(figsize=(9, 3))
     plt.pcolormesh(t, f, sgr_log)
@@ -67,15 +64,15 @@ def impulse_response(b, a, index):
     plt.tight_layout()
 
 
-def task_7(found_frekvencies, sample_frekvency, signal):
+def task_7(found_frequencies, sample_frequency, signal):
     data = signal
     index = 1
-    for frequency in found_frekvencies:
-        data = filter_function(frequency, sample_frekvency, data, index)
+    for frequency in found_frequencies:
+        data = filter_function(frequency, sample_frequency, data, index)
         index += 1
     print(f'{data}')
     print(f'data done')
-    task_7_spectogram(data, sample_frekvency)
+    task_7_spectogram(data, sample_frequency)
     return data
 
 
