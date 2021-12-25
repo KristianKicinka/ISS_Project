ca18bcf3fbe3b2fa8371857de105303e63d00b0c
KristianKicinka <kristian.kicinka@gmail.com>
2021-12-25 22:06:50 +0100

    after update task_08 and task_10 file

--- src/task_10.py
+++ src/task_10.py
@@ -1,7 +1,8 @@
 import soundfile
 
-def task_10(data,sample_frekvency):
-    soundfile.write('audio/clean_bandstop.wav', data, sample_frekvency)
+
+def task_10(data,sample_frequency):
+    soundfile.write('audio/clean_bandstop.wav', data, sample_frequency)
     print('Process done')
 
 
