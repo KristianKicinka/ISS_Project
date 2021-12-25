e4303a566f4afca25ceb1e0a59ed8b8e45c0dfa1
KristianKicinka <kristian.kicinka@gmail.com>
2021-12-25 21:49:14 +0100

    after update task_02,task_03 file

--- src/task_02.py
+++ src/task_02.py
@@ -1,4 +1,4 @@
-import matplotlib as matplotlib
+
 import numpy as np
 import matplotlib.pyplot as plt
 import soundfile as sound_file
@@ -9,16 +9,16 @@ def get_frames():
     return frames
 
 
-def task_2(frames, time_line, sample_frekvency):
+def task_2(frames, time_line, sample_frequency):
     # choose correct frame from signal
     my_frame = frames[7]
-    show_frames_graph(frames, my_frame, time_line, sample_frekvency)
+    show_frames_graph(frames, my_frame, time_line, sample_frequency)
     return my_frame
 
 
-def show_frames_graph(frames, my_frame, time_line, sample_frekvency):
+def show_frames_graph(frames, my_frame, time_line, sample_frequency):
     plt.figure(figsize=(30, 8))
-    time_line = np.arange(my_frame.size) / sample_frekvency
+    time_line = np.arange(my_frame.size) / sample_frequency
     plt.plot(time_line, my_frame)
     plt.ylabel('Hodnota []')
     plt.gca().set_xlabel('Čas [s]')
--- src/task_03.py
+++ src/task_03.py
@@ -1,4 +1,4 @@
-import matplotlib as matplotlib
+
 import matplotlib.pyplot as plt
 import numpy as np
 
