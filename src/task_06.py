aaede533a7623ffd0dc9e1f3d9e5ad5e4861ccd0
KristianKicinka <kristian.kicinka@gmail.com>
2021-12-25 22:00:30 +0100

    after update task_06 file

--- src/task_06.py
+++ src/task_06.py
@@ -1,24 +1,24 @@
+
 import numpy as np
 import soundfile
 from matplotlib import pyplot as plt
 from scipy.signal import spectrogram
 
 
-def task_6(frekvencies, sample_frekvency, signal):
-    norm_frkv = np.size(signal) / sample_frekvency
-    step = 1/sample_frekvency
+def task_6(frequencies, sample_frequency, signal):
+    norm_frequency = np.size(signal) / sample_frequency
+    step = 1/sample_frequency
     cos_signal = np.empty(np.size(signal))*0.0
-    for frekvency in frekvencies:
-        cos_signal = cos_signal + np.cos(np.arange(0,norm_frkv,step)*(2*np.pi*frekvency))
+    for frequency in frequencies:
+        cos_signal = cos_signal + np.cos(np.arange(0, norm_frequency, step)*(2*np.pi*frequency))
 
     print(f'cos : {cos_signal}')
-    task_6_spectogram(cos_signal, sample_frekvency)
-    soundfile.write('audio/4cos.wav',cos_signal,sample_frekvency)
+    task_6_spectogram(cos_signal, sample_frequency)
+    soundfile.write('audio/4cos.wav', cos_signal, sample_frequency)
 
 
-
-def task_6_spectogram(signal,sample_frekvency):
-    f, t, sgr = spectrogram(signal, sample_frekvency, nperseg=1024, noverlap=512)
+def task_6_spectogram(signal, sample_frequency):
+    f, t, sgr = spectrogram(signal, sample_frequency, nperseg=1024, noverlap=512)
     sgr_log = 10 * np.log10(pow(np.abs(sgr), 2))
     plt.figure(figsize=(9, 3))
     plt.pcolormesh(t, f, sgr_log)
