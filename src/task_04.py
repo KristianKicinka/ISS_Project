6ed273ba9020aa5d9f245d5a2f9776ae9d86a4be
KristianKicinka <kristian.kicinka@gmail.com>
2021-12-25 21:50:46 +0100

    after update task_04 file

--- src/task_04.py
+++ src/task_04.py
@@ -1,11 +1,11 @@
-import matplotlib as matplotlib
+
 import numpy as np
 from scipy.signal import spectrogram
 import matplotlib.pyplot as plt
 
 
-def task_4(signal, sample_frekvency):
-    f, t, sgr = spectrogram(signal, sample_frekvency, nperseg=1024, noverlap=512)
+def task_4(signal, sample_frequency):
+    f, t, sgr = spectrogram(signal, sample_frequency, nperseg=1024, noverlap=512)
     sgr_log = 10 * np.log10(pow(np.abs(sgr), 2))
     plt.figure(figsize=(9, 3))
     plt.pcolormesh(t, f, sgr_log)
